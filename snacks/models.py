5a505de571ccecab7432802e449c5cff0f06aa15
dialaabulkhail <diala.sh.98@gmail.com>
2022-05-08 22:53:41 +0300

    lab 26

--- snacks/models.py
+++ snacks/models.py
@@ -1,3 +1,13 @@
+from pydoc import describe
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    describtion = models.TextField()
+    price = models.IntegerField()
+    count = models.IntegerField()
+
+
+    def __str__(self):
+        return self.name
